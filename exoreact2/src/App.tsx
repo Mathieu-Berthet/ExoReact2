55eb991246963cdf7a23ad1ad66b2a597b61620b
Mathieu <berthetmathieu@free.fr>
2023-06-01 16:31:21 +0200

    end day

--- exoreact2/src/App.tsx
+++ exoreact2/src/App.tsx
@@ -1,25 +1,73 @@
-import React from 'react';
+import React, {useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { Link, Route } from "wouter";
 
 function App() {
+
+  const [music, setMusic] = useState([]);
+  const [isError, setIsError] = useState(false);
+  const [listAlbum, setListAlbum] = useState(false);
+  const [albumInfo, setAlbumInfo] = useState(true);
+  const [authorInfo, setAuthorInfo] = useState(true);
+
+  let token = "dKXDMyIEylUQdtqUvuJNHHYcBTqKxFtHJWQGKrqo";
+
+  useEffect(() : void => {
+      const fetchData = async () : Promise<void> => {
+          setIsError(false);
+          try {
+              const response = await axios.get("https://api.discogs.com/database/search?q=Nirvana",
+                  {
+                      headers: {
+                          Authorization: `Bearer ${token}`
+                      }
+                  });
+
+              let result = response.data;
+              console.log(result);
+              //setMusic(result);
+          }
+          catch (error)
+          {
+              setIsError(true);
+              console.log(error);
+          }
+      };
+      fetchData();
+  }, []);
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div>
+            <Link href="/users/1">
+                <a className="link">Profile</a>
+            </Link>
+
+            <Route path="/about">About Us</Route>
+            <Route path="/users/:name">{(params) => <div>Hello, {params.name}!</div>}</Route>
+            <Route path="/inbox" component={App} />
+        </div>
+
+      <div id="listAlbum" hidden={listAlbum}>
+          {/*{music.map((item) => (
+              <div>
+                  <img src="" alt="imageAlbum" />
+                  <p>{item}</p>
+                  <p>{item}</p>
+              </div>
+          ))}*/}
+      </div>
+      <div id="album" hidden={albumInfo}>
+
+      </div>
+
+      <div id="author" hidden={authorInfo}>
+
+      </div>
     </div>
+
   );
 }
 
